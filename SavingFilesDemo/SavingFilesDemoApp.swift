f983f73fea3fb7255104f2923aa563b486b74e6c
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2021-08-08 21:16:09 +0200

    Initial Commit

---
+++ SavingFilesDemo/SavingFilesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingFilesDemoApp.swift
+//  SavingFilesDemo
+//
+//  Created by Ahmed M. Hassan on 08/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SavingFilesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
